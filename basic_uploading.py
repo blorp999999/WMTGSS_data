543eaa16e2d67f73599975979e46862068a7272c
EC2 Default User <ec2-user@ip-172-31-26-69.eu-west-2.compute.internal>
2022-01-27 12:09:03 +0000

    Basic uploading + some security features

---
+++ basic_uploading.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import imghdr
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024 # Security feature, limits the filesize of uploaded files to 200MB to prevent server overload through upload of massive files
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.mp4', '.doc', '.docx', '.pdf', '.odt', '.html', '.ppt', '.pptx'] # Security feature, limits allowed files to previously specified ones (prevents upload of code files, etc)
+app.config['UPLOAD_PATH'] = 'uploads'
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.route('/')
+def index():
+    return render_template('upload_form.html')
+    
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename) # Uses the werkzeug secure_filename to reduce the filename of the uploaded to a flat filename - reduces vectors for filename based attacks
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream):
+            Flask.abort(400) # If file is not in given filetypes, aborts and outputs a HTTP 400 error (bad request)
+        
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
+    
