39c326713d4a85a85be5f5193ce8a6e9a3a3e2bf
EC2 Default User <ec2-user@ip-172-31-26-69.eu-west-2.compute.internal>
2022-01-27 14:26:38 +0000

    Signup

--- login.py
+++ login.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
 from main import app, db
 
 auth = Blueprint('auth', __name__)
@@ -15,6 +17,30 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+    
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    
+    # Validates and adds user to the database
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email).first() # Checks if the user email is already within the database
+    
+    if user: # If the user already exists, redirect back to the signup page so the user can try again
+        return redirect(url_for('auth.signup'))
+    
+    # Creates a new user with the form data
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256')) # Hashes the password so a plaintext version isn't saved
+    
+    # Add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    
+    
+    # Once signup is completed, redirects the user to the login page
+    return redirect(url_for('auth.login'))
 
 @auth.route('logout')
 def logout():
--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from main import db
 
 class User(db.model):
     id = db.Column(db.Integer, primary_key=True) # User ID, used as the primary key within the user database
-    email = db.Column(db.String, unique=True) # User Email, has to be hashed and unique
+    email = db.Column(db.String, unique=True) # User Email, has to be unique
     password = db.Column(db.String(100)) # User Password, has to be hashed
     name = db.Column(db.String(1000)) # User name
