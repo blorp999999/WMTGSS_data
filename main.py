567d49d7769fe24fd064b00bec4685849dfe89a4
EC2 Default User <ec2-user@ip-172-31-26-69.eu-west-2.compute.internal>
2022-01-27 15:13:42 +0000

    Added profile page (very basic currently)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
+from flask_login import login_required, current_user
 from . import db
 
 
@@ -11,5 +12,6 @@ def index():
     return render_template('index.html')
 
 @Flask.main.route('/profile')
+@login_required
 def profile():
-    return 'Profile'
+    return render_template('profile.html', name = current_user.name)
